e00ae5db7352e52e827e1481afafef472d811953
BaTianCheng <cheng_wei_njut@163.com>
2018-11-07 15:09:32 +0800

    功能开发

--- src/main/java/com/cw/guass2/controller/ManagerController.java
+++ src/main/java/com/cw/guass2/controller/ManagerController.java
@@ -1,6 +1,7 @@
 package com.cw.guass2.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
